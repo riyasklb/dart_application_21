9b118c92c4a540cf99452d9b9c678ffd0279a7db
riyasklb <riyasklb89@gmail.com>
2023-01-16 12:43:13 +0530

    first commit

---
+++ lib/dart_application_21.dart
@@ -0,0 +1,21 @@
+import 'package:dart_application_21/dart_application_21.dart'
+    as dart_application_21;
+
+void main(List<String> arguments) {
+  var month = 4;
+  switch (month) {
+    case 2:
+      print('riyas');
+      break;
+    case 1:
+      print('joan');
+      continue reason;
+    reason:
+    case 3:
+      print('sabu');
+      break;
+    case 4:
+      print('murshi');
+      break;
+  }
+}
